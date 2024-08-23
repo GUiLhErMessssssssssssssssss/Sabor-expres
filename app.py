71e831d0be1342528fb8fe24dabd01b45a84960e
Guilherme Morales <140725028+GUiLhErMessssssssssssssssss@users.noreply.github.com>
2024-08-23 07:45:02 -0300

    Add files via upload
    começo

---
+++ app.py
@@ -0,0 +1,9 @@
+print("""Jm viraçaria\n""")
+
+print("1 Cadastrar funcionario")
+print("2. Lita e funcionarios")
+print("3. Ativarr matricula")
+print("4.Sair")
+
+opcao_ecolhida = input("Ecolha uma opção")
+print("Voçê ecolheu a opção:",opcao_ecolhida)
